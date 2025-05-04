50a9ffa58739899238f6a7ccc13698a54dbce95e
j-mugerwa <jmugerwa2020@gmail.com>
2025-05-04 15:31:22 +0300

    Pic

--- Frontend/src/pages/HomePage.js
+++ Frontend/src/pages/HomePage.js
@@ -3,16 +3,21 @@ import { Box, Grid, Typography, Button, TextField, Paper } from "@mui/material";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer"; // Import Footer
 import Actions from "../components/Actions";
-import cloud from "../assets/cloud.jpg"
-//import Navbar from "../components/NavBar";
+import cloud from "../assets/cloud.jpg";
+
 const HomePage = () => {
   return (
     <Box>
-      <Grid container alignItems="center" justifyContent="center" sx={{ minHeight: "80vh", px: 4 }}>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "80vh", px: 4 }}
+      >
         <Grid item xs={12} md={6}>
           <img
             src={cloud}
-            alt="Cloud Management Config."
+            alt="Attendance Management."
             style={{ width: "100%", borderRadius: 8 }}
           />
         </Grid>
@@ -22,7 +27,7 @@ const HomePage = () => {
             <Typography variant="h5" gutterBottom>
               HOME
             </Typography>
-                <Actions />
+            <Actions />
           </Paper>
         </Grid>
       </Grid>
